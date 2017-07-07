6c6c46193774cb620fa3c35c8f353b0f03ce12f6
meduardasouza <megs.dlt12@gmail.com>
2017-07-07 09:00:10 -0300

    Create codigo.java

---
+++ codigo.java
@@ -0,0 +1,40 @@
+ int a = 0, b, c, x = 0, y = 1, z, w = 0, soma = 0, i = 0;
+
+        while (a < 4) {
+
+            System.out.print("Apresentar os numeros da série Fibonacci ===== 1\n");
+            System.out.print("Apresentar numeros primos ===== 2\n");
+            System.out.print("Apresentar numeros perfeitos ===== 3\n");
+            System.out.print("Sair do programa ===== 4\n");
+            JOptionPane.showInputDialog("Confirme a sua opção: ");
+        }
+
+        System.out.print(i + "\n");
+        //O valor de i e depois pula uma linha;
+        System.out.print("O valor de i é " + i);
+        // mostra a frase "O valor de i é " (tem que te um espaço para o valor da variavel depois do (é))e depois o valor de i 
+        System.out.print("i é " + i + "! =D \n");
+        System.out.println(soma);
+        
+        
+        
+        String JOptionPane.showInputDialog (string);
+        String oqOUsuarioDigitar  = JOptionPane.showInputDialog();
+        
+        a = Integer.parseInt(oqOUsuarioDigitar);
+        
+        scanf("%c", c);
+        // Conversão:
+        char umChar = oqOUsuarioDigitar.charAt(0);
+        // "Conversão", pegar o valor de uma string e guardar numa variavel int;
+        String yolo = JOptionPane.showInputDialog();
+        int foo = integer.parseInt(yolo);
+        // "Conversão", pegar o valor de uma string e guardar numa variavel float;
+        float baz = Float.parseFloat(yolo);
+        // "Conversão", pegar o valor de uma string e guardar numa variavel char;
+        char bar = yolo.charAt(0); // "(0)" = a primeira possisão do char;
+        System.out.println("Yolo = " + foo);
+        
+    }
+
+}
